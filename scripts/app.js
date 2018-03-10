1226d659594fb66d41bd43479387029141196f11
Shawn Wilkinson <me@super3.org>
2018-03-10 11:39:03 -0500

    Download and Mine

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,29 @@
+const base58check = require('base58check');
+const got = require("got");
+const fs = require('fs');
+const util = require("util");
+const extract = util.promisify(require('extract-zip'));
+
 const Vue = require("vue/dist/vue.common.js");
 const { spawn } = require('child_process');
-const base58check = require('base58check');
+
+const miners = require("./../miners.js");
 let miner;
 
 const app = new Vue({
   el: "main",
   data: {
+    minerInfo: miners[process.platform],
     address: localStorage.getItem('address') || "",
     output: "",
-    isMining: false
+    isMining: false,
+    downloaded: false
   },
   methods: {
     startMining() {
       localStorage.setItem('address', this.address);
 
-      miner = spawn('nheqminer_nanopool_v0.4b/nheqminer.exe',
-      ['-l',  'zec-us-east1.nanopool.org:6666',  '-u',  `t1YtcRXgoDsVj6sDhGA71sgdDLoR9Q1QcnL/${this.address}`,
-       '-cd', '0', '-p', 'x']);
+      miner = spawn(__dirname + "/../miner/" + this.minerInfo.binary, this.minerInfo.arguments(this.address));
 
       miner.stdout.on("data", data => {
        this.output += data;
@@ -42,5 +49,23 @@ const app = new Vue({
         return false;
       }
     }
-  }
+  },
 });
+
+async function created() {
+  const minerInfo = this.minerInfo;
+
+  const minerFolder = __dirname + "/../miner";
+  const zipPath = __dirname + "/../miner.zip";
+  const zipStream = fs.createWriteStream(zipPath);
+
+  got.stream(minerInfo.url)
+    .pipe(zipStream);
+
+  zipStream.on("close", async () => {
+      await extract(zipPath, {dir: minerFolder});
+      this.downloaded = true;
+  });
+}
+
+created.call(app);
