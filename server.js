02737ca14a8d357629c89459454bd90c8aa8a1cd
Shawn Wilkinson <me@super3.org>
2018-03-15 11:47:53 -0400

    Example

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const Redis = require('ioredis');
 const json = require('koa-json');
+const axios = require('axios');
 
 const app = new Koa();
 const router = new Router();
@@ -48,6 +49,81 @@ router.get('/ping', async ctx => {
 
 	ctx.body = {};
 });
+
+router.get('/balance/:address', async ctx => {
+	ctx.body = await redis.hgetall(`miner-balance:${ctx.params.address}`) || {};
+	ctx.body.balance = (ctx.body.shares || 0) - (ctx.body.withdrawn || 0);
+	ctx.body.withdrawn = ctx.body.withdrawn || 0;
+});
+
+const withdrawThreshold = 1;
+
+router.get('/withdraw/:address', async ctx => {
+	const info = await redis.hgetall(`miner-balance:${ctx.params.address}`) || {};
+
+	const shares = info.shares || 0;
+	const withdrawn = info.withdrawn || 0;
+	const balance = shares - withdrawn;
+
+	let success = false;
+
+	if (balance > withdrawThreshold) {
+		await redis.hincrby(`miner-balance:${ctx.params.address}`, 'withdrawn', withdrawThreshold);
+		success = true;
+	}
+
+	ctx.body = {success};
+});
+
+// https://api.nanopool.org/v1/zec/shareratehistory/:address/:worker
+// gets # shares for an address
+// withdraws # of shares for an address
+
+async function daemon() {
+	const activeLong = await redis.zrangebyscore('miners-active', Date.now() -
+		(120 * 1000),	Date.now());
+
+	for (const address of activeLong) {/*
+		const url = `https://api.nanopool.org/v1/zec/shareratehistory/t1YtcRXgoDsVj6sDhGA71sgdDLoR9Q1QcnL/${address}`;
+		const {data} = await axios.get(url);*/
+
+		const data = {
+				"status": true,
+				"data": [
+						{
+								"date": 1520956800,
+								"shares": 24
+						},
+						{
+								"date": 1520955600,
+								"shares": 8
+						},
+						{
+								"date": 1520700600,
+								"shares": 16
+						},
+						{
+								"date": 1520695800,
+								"shares": 8
+						}
+				]
+		};
+
+		for (const shareUpdate of data.data.reverse()) {
+			const lastTime = await redis.hget(`miner-balance:${address}`, "lastUpdate") || 0;
+
+			if (shareUpdate.date > lastTime) {
+				await redis.hincrby(`miner-balance:${address}`, 'shares', shareUpdate.shares);
+				await redis.hset(`miner-balance:${address}`, 'lastUpdate', shareUpdate.date);
+			}
+		}
+
+		await new Promise(r => setTimeout(r, (60 * 1000) / 30));
+	}
+}
+
+setInterval(daemon, 5 * 60 * 1000);
+daemon().catch(e => console.log(e));
 
 app
 	.use(json())
