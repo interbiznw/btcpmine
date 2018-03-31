c8d9f852a6d7602bbdb246fc0f32c71940afb4a6
Shawn Wilkinson <me@super3.org>
2018-03-31 14:28:46 -0400

    Auto Update

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const {app, BrowserWindow} = require('electron');
 
 let win;
 
+const {autoUpdater} = require('electron-updater');
+const {version} = require('./package.json');
+
+const server = 'http://zfaucet.org:3005';
+const feed = `${server}/update/${process.platform}/${version}`;
+
+autoUpdater.setFeedURL(feed);
+
 app.on('ready', () => {
 	win = new BrowserWindow({width: 800, height: 600});
 
