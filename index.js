86bf0a8cb3540cc6eb8353f13da58ef4c6bc8819
Shawn Wilkinson <me@super3.org>
2018-04-01 14:49:38 -0400

    Remove Update Code

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
 const url = require('url');
 const path = require('path');
-const {app, BrowserWindow, dialog} = require('electron');
+const {app, BrowserWindow} = require('electron');
 
 let win;
-
-const {autoUpdater} = require('electron-updater');
-const {version} = require('./package.json');
-
-const server = 'http://zfaucet.org:3005';
-const feed = `${server}/update/${process.platform}/${version}`;
-
-autoUpdater.setFeedURL(feed);
-autoUpdater.checkForUpdates(); // check on launch
-
-setInterval(() => {
-	autoUpdater.checkForUpdates();
-}, 60 * 1000);
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-		type: 'info',
-		buttons: ['Restart', 'Later'],
-		title: 'Application Update',
-		message: process.platform === 'win32' ? releaseNotes : releaseName,
-		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-	};
-
-	dialog.showMessageBox(dialogOpts, response => {
-		if (response === 0) autoUpdater.quitAndInstall();
-	});
-});
-
-autoUpdater.on('error', message => {
-	console.error('There was a problem updating the application');
-	console.error(message);
-});
 
 app.on('ready', () => {
 	win = new BrowserWindow({width: 800, height: 600});
