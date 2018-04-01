70cce19cc629cdbf51642e12dafc9bfd98d07bfb
Shawn Wilkinson <me@super3.org>
2018-04-01 14:11:42 -0400

    Assume Update Code Works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const url = require('url');
 const path = require('path');
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, dialog} = require('electron');
 
 let win;
 
@@ -11,6 +11,30 @@ const server = 'http://zfaucet.org:3005';
 const feed = `${server}/update/${process.platform}/${version}`;
 
 autoUpdater.setFeedURL(feed);
+autoUpdater.checkForUpdates(); // check on launch
+
+setInterval(() => {
+	autoUpdater.checkForUpdates();
+}, 60 * 1000);
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+	const dialogOpts = {
+		type: 'info',
+		buttons: ['Restart', 'Later'],
+		title: 'Application Update',
+		message: process.platform === 'win32' ? releaseNotes : releaseName,
+		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+	};
+
+	dialog.showMessageBox(dialogOpts, response => {
+		if (response === 0) autoUpdater.quitAndInstall();
+	});
+});
+
+autoUpdater.on('error', message => {
+	console.error('There was a problem updating the application');
+	console.error(message);
+});
 
 app.on('ready', () => {
 	win = new BrowserWindow({width: 800, height: 600});
