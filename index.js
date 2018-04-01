fbe42b09a9afc8b19596dbe5f35ba686bfc0cfe1
Shawn Wilkinson <me@super3.org>
2018-04-01 15:54:17 -0400

    Update?

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
 const url = require('url');
 const path = require('path');
-const {app, BrowserWindow, dialog, autoUpdater} = require('electron');
+const {app, BrowserWindow} = require('electron');
 
 let win;
 
-const {version} = require('./package.json');
+const updater = require('electron-simple-updater');
 
-const server = 'http://zfaucet.org:3005';
-const feed = `${server}/update/${process.platform}/${version}`;
-
-autoUpdater.setFeedURL(feed);
-// autoUpdater.checkForUpdates(); // check on launch
-
-setInterval(() => {
-	autoUpdater.checkForUpdates();
-}, 60 * 1000);
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-		type: 'info',
-		buttons: ['Restart', 'Later'],
-		title: 'Application Update',
-		message: process.platform === 'win32' ? releaseNotes : releaseName,
-		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-	};
-
-	dialog.showMessageBox(dialogOpts, response => {
-		if (response === 0) autoUpdater.quitAndInstall();
-	});
-});
-
-autoUpdater.on('error', message => {
-	console.error('There was a problem updating the application');
-	console.error(message);
-});
+updater.init('https://gist.githubusercontent.com/super3/35a9d706b9f70ef236eb04d472431cf5/raw/13bac8aace03e8bc1859ddb3b8cea2882d1d5a38/updates.json');
 
 app.on('ready', () => {
 	win = new BrowserWindow({width: 800, height: 600});
