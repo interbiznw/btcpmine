2be5bd67c60b64cc270407f559d530070a66d31a
Shawn Wilkinson <me@super3.org>
2018-04-02 10:37:02 -0400

    Better Pinger

--- server.js
+++ server.js
@@ -19,17 +19,19 @@ function isAddress(address) {
 }
 
 router.get('/', async ctx => {
-	const active = await redis.zrangebyscore('miners-active', Date.now() - (60 * 1000),
+	const timeSince = typeof ctx.query.since === 'undefined' ?
+		Date.now() - (60 * 1000) : Date.now() - Number(ctx.query.since);
+
+	const active = await redis.zrangebyscore('miners-active', timeSince,
 		Date.now());
 
 	ctx.body = {
-		message: 'Hello, World!',
-		counter: await redis.incr('counter'),
 		active: await Promise.all(active.map(async address => {
-			const {hashRate} = await JSON.parse(await redis.lindex(`miner:${address}`, 0));
+			const {hashRate, date} = await JSON.parse(await redis.lindex(`miner:${address}`, 0));
 			return {
 				address,
-				reportedHashRate: hashRate
+				reportedHashRate: hashRate,
+				lastSeen: date
 			};
 		}))
 	};
@@ -47,45 +49,45 @@ router.get('/ping', async ctx => {
 	ctx.body = {};
 });
 
-router.get('/balance/:address', async ctx => {
-	ctx.body = await redis.hgetall(`miner-balance:${ctx.params.address}`) || {};
-	ctx.body.balance = (ctx.body.shares || 0) - (ctx.body.withdrawn || 0);
-	ctx.body.withdrawn = ctx.body.withdrawn || 0;
-});
-
-const withdrawThreshold = 1;
-
-router.get('/withdraw/:address', async ctx => {
-	const info = await redis.hgetall(`miner-balance:${ctx.params.address}`) || {};
-
-	const shares = info.shares || 0;
-	const withdrawn = info.withdrawn || 0;
-	const balance = shares - withdrawn;
-
-	let success = false;
-
-	if (balance > withdrawThreshold) {
-		await redis.hincrby(`miner-balance:${ctx.params.address}`, 'withdrawn', withdrawThreshold);
-		success = true;
-	}
-
-	ctx.body = {success};
-});
+// router.get('/balance/:address', async ctx => {
+// 	ctx.body = await redis.hgetall(`miner-balance:${ctx.params.address}`) || {};
+// 	ctx.body.balance = (ctx.body.shares || 0) - (ctx.body.withdrawn || 0);
+// 	ctx.body.withdrawn = ctx.body.withdrawn || 0;
+// });
+//
+// const withdrawThreshold = 1;
+//
+// router.get('/withdraw/:address', async ctx => {
+// 	const info = await redis.hgetall(`miner-balance:${ctx.params.address}`) || {};
+//
+// 	const shares = info.shares || 0;
+// 	const withdrawn = info.withdrawn || 0;
+// 	const balance = shares - withdrawn;
+//
+// 	let success = false;
+//
+// 	if (balance > withdrawThreshold) {
+// 		await redis.hincrby(`miner-balance:${ctx.params.address}`, 'withdrawn', withdrawThreshold);
+// 		success = true;
+// 	}
+//
+// 	ctx.body = {success};
+// });
 
 // https://api.nanopool.org/v1/zec/shareratehistory/:address/:worker
 // gets # shares for an address
 // withdraws # of shares for an address
 
-async function daemon() {
-	const url = `https://api-zcash.flypool.org/miner/t1YtcRXgoDsVj6sDhGA71sgdDLoR9Q1QcnL/dashboard`;
-	const {data: {data}} = await axios.get(url);
-
-	for (const {worker, validShares} of data.workers)
-		await redis.hset(`miner-balance:${worker}`, 'shares', validShares);
-}
-
-setInterval(() => daemon().catch(e => console.log(e)), 5 * 60 * 1000);
-daemon().catch(e => console.log(e));
+// async function daemon() {
+// 	const url = `https://api-zcash.flypool.org/miner/t1YtcRXgoDsVj6sDhGA71sgdDLoR9Q1QcnL/dashboard`;
+// 	const {data: {data}} = await axios.get(url);
+//
+// 	for (const {worker, validShares} of data.workers)
+// 		await redis.hset(`miner-balance:${worker}`, 'shares', validShares);
+// }
+//
+// setInterval(() => daemon().catch(e => console.log(e)), 5 * 60 * 1000);
+// daemon().catch(e => console.log(e));
 
 app
 	.use(json())
