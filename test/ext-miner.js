24859926696ae8083496e482b112eb7b13db338c
Monty Anderson <montydev@protonmail.com>
2018-04-02 12:13:01 +0100

    Error Handling, Tests, Linux Fixes

--- test/ext-miner.js
+++ test/ext-miner.js
@@ -3,8 +3,21 @@ const extMiner = require('../lib/ext-miner');
 
 describe('ext-miner', () => {
 	describe('install()', () => {
-		it('should run without fail', async () => {
+		it('should run without fail', async function () {
+			this.timeout(10 * 1000);
 			await extMiner.install();
+		});
+	});
+
+	describe('start()', () => {
+		it('should start', async () => {
+			await extMiner.start('t1hASvMj8e6TXWryuB3L5TKXJB7XfNioZP3', 'CPU');
+		});
+	});
+
+	describe('stop()', () => {
+		it('should stop', async () => {
+			await extMiner.stop();
 		});
 	});
 });
