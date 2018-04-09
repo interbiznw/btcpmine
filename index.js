27a10c4fc753e390867242ecc98bb02e96afb572
Shawn Wilkinson <me@super3.org>
2018-04-09 10:39:29 -0400

    Updated Template

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 app.on('ready', () => {
-	win = new BrowserWindow({width: 800, height: 700});
+	win = new BrowserWindow({width: 800, height: 800});
 
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
