300ffabff0582d189c30bdf3af25d2d0e6380c28
Your Name <j.owens@interbiznw.com>
2018-10-18 12:30:13 -0700

    fix appveyor build errors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 app.on('ready', () => {
-	win = new BrowserWindow({width: 900, height: 800})
+	win = new BrowserWindow({width: 900, height: 800});
 	win.setMenu(null);
 
 	win.loadURL(url.format({
