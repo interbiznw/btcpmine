0aacfc22bbe057ae63a24e7fc8b6e7a36a85dd0f
Your Name <j.owens@interbiznw.com>
2018-10-18 18:23:39 -0700

    fix updater and bump ver

--- components/update-check.js
+++ components/update-check.js
@@ -5,7 +5,7 @@ const {version} = require('../package.json');
 
 module.exports = Vue.component('update-check', {
 	template: `
-	<div class="alert alert-primary alert-dismissible fade show"
+	<div class="alert alert-warning alert-dismissible fade show"
 		role="alert" v-if="version !== latestVersion">
 		<strong>Out of date!</strong> Please update btcpmine
 		<a style="color: inherit; text-decoration: underline;"
@@ -21,19 +21,30 @@ module.exports = Vue.component('update-check', {
 		latestVersion: version
 	}),
 	async created() {
+
+		const startupCheckNewVersion = async () => {
+			const packageUrl = 'https://raw.githubusercontent.com/interbiznw/btcpmine/master/package.json';
+			const {data: {version}} = await axios.get(packageUrl);
+			this.latestVersion = version;
+			console.log("firstcheck on load");
+		};
+
+		startupCheckNewVersion();
+
 		const getLatestVersion = async () => {
 			const packageUrl = 'https://raw.githubusercontent.com/interbiznw/btcpmine/master/package.json';
 			const {data: {version}} = await axios.get(packageUrl);
 			this.latestVersion = version;
+			console.log("subsequent timed checks")
 		};
 
 		// check every 15 minutes
-		setInterval(getLatestVersion, 1 * 60 * 100);
+		setInterval(getLatestVersion, 15 * 60 * 1000);
 	},
 	methods: {
 		openExternal(url) {
 			shell.openExternal(url);
 		}
-	},
+	}
 
 });
